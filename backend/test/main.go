febc482fe05c8bf1e7e3ebadb20f10b28f3dcf61
Delo <delo_wdl@163.com>
2020-03-17 22:11:18 +0800

    代码备份

--- backend/test/main.go
+++ backend/test/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+type File struct {
+	name string
+	size int
+	attr struct {
+		perm  int
+		owner int
+	}
+}
+
+type User struct {
+	name string
+	age  int
+}
+
+func main() {
+	f := File{
+		name: "test.txt",
+		//size: 1024,
+	}
+	fmt.Println(f)
+	u1 := User{age: 1}
+	fmt.Println(u1)
 }
